1d49e839e874f7c347933bafe0a591058a554c36
Baptiste-Ntr <98717656+Baptiste-Ntr@users.noreply.github.com>
2022-09-29 23:19:58 +0200

    Update submit

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,18 @@
-function alertpost() {
-  var alertcontainer = document.getElementById("alertconainer");
-  var alertsend = document.getElementById("alertsend");
+var alertcontainer = document.getElementById("alertcontainer");
+var alertsend = document.getElementById("alertsend");
+var submithide = document.getElementById("submithide");
 
+function alertpost() {
+  alertcontainer.style.animation = "alertanim 0.3s ease";
   alertcontainer.style.display = "flex";
+
+  setTimeout(function () {
+    alertcontainer.style.animation = "alertanimreverse 0.3s ease";
+    setTimeout(function () {
+      alertcontainer.style.display = "none";
+    }, 280);
+    setTimeout(function () {
+      submithide.click();
+    }, 700);
+  }, 3000);
 }

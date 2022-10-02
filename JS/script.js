7f42f4495ba6d59b54a79671ef33229b3fea9ea8
Baptiste-Ntr <98717656+Baptiste-Ntr@users.noreply.github.com>
2022-10-02 23:46:17 +0200

    update css

--- JS/script.js
+++ JS/script.js
@@ -4,17 +4,13 @@ var submithide = document.getElementById("submithide");
 // getElementsByClassName retourne un tableau avec tous les inputs dedans
 var require = document.getElementsByClassName("require");
 
-var test = document.getElementById("test").innerText.length;
-
-// if (document.getElementsById("require").)
-
 function alertpost() {
   for (var i = 0; i < require.length; i++) {
     if (require[i].value == "") {
       console.log(require[i].value);
       require[i].classList.add = "invalid";
       alertsend.innerHTML =
-        "<p style='font-size='25px;''> Des champs obligatoire ne sont pas remplis </p>";
+        "<p> Des champs obligatoire ne sont pas remplis </p>";
       alertcontainer.style.animation = "alertanim 0.3s ease";
       alertcontainer.style.display = "flex";
 
@@ -51,42 +47,4 @@ function alertpost() {
       }, 2000);
     }
   }
-
-  // if (require.item.length == 0) {
-  //   alertsend.innerText = "<p> Le formulaire est invalide </p>";
-  //   alertcontainer.style.animation = "alertanim 0.3s ease";
-  //   alertcontainer.style.display = "flex";
-
-  //   alertsend.scrollIntoView({
-  //     behavior: "smooth",
-  //     block: "center",
-  //     inline: "nearest",
-  //   });
-
-  //   setTimeout(function () {
-  //     alertcontainer.style.animation = "alertanimreverse 0.3s ease";
-  //     setTimeout(function () {
-  //       alertcontainer.style.display = "none";
-  //     }, 280);
-  //   }, 1500);
-  // } else if (require.item.length >= 1) {
-  //   alertcontainer.style.animation = "alertanim 0.3s ease";
-  //   alertcontainer.style.display = "flex";
-
-  //   alertsend.scrollIntoView({
-  //     behavior: "smooth",
-  //     block: "center",
-  //     inline: "nearest",
-  //   });
-
-  //   setTimeout(function () {
-  //     alertcontainer.style.animation = "alertanimreverse 0.3s ease";
-  //     setTimeout(function () {
-  //       alertcontainer.style.display = "none";
-  //     }, 280);
-  //     setTimeout(function () {
-  //       submithide.click();
-  //     }, 700);
-  //   }, 2000);
-  // }
 }
